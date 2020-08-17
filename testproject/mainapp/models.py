294e73fe9af05cfbcb6987cb024a7b1f6fa1ed6d
PyTimDeveloper <PyTimDev@gmail.com>
2020-08-17 15:28:32 +0300

    UPD

--- testproject/mainapp/models.py
+++ testproject/mainapp/models.py
@@ -3,13 +3,13 @@ from django.db import models
 class Post(models.Model):
     name = models.CharField(verbose_name='Заголовок: ', max_length=200)
     slug = models.SlugField(unique=True)
-    img = models.ImageField(verbose_name='Изображение: ', upload_to='static/img/posts/')
-    fdesc = models.TextField(verbose_name='Краткое описание', max_length=500)
-    sdesc = models.TextField(verbose_name='Полное описание')
+    img = models.ImageField(verbose_name='Зображення: ', upload_to='static/img/posts/')
+    fdesc = models.TextField(verbose_name='Коротке описання', max_length=500)
+    sdesc = models.TextField(verbose_name='Повний опис')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Новость'
-        verbose_name_plural = 'Новости'
+        verbose_name = 'Новина'
+        verbose_name_plural = 'Новини'
